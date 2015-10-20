67dafea915af64fb5b6ac804d111b9d98fe4b065
DustinY <Snowman321>
2015-10-19 20:33:21 -0700

    Samples
    Samples for references on basic python syntax

---
+++ Assignment_1/Samples/sample1_3.5.py
@@ -0,0 +1,49 @@
+import random
+import math
+
+
+def hello_World():
+    print('Hello World')
+
+
+def greatest_Num(num1, num2):
+    if(num1 > num2):
+        print(num1)
+    else:
+        print(num2)
+
+
+def get_Greatest_Num(num1, num2):
+    if(num1 > num2):
+        return num1
+    else:
+        return num2
+
+
+def roll_Dice():
+    rolled_num = math.floor(random.randrange(1, 7))
+    print(rolled_num)
+    return rolled_num
+
+
+def main():
+    loop = True
+    while(loop):
+        choice = input('\n1. Print "Hello World"\n2. Print Greatest Number\n'
+                       '3. Roll a dice (get a number between 1 and 6)\n'
+                       '4. Exit Program\n>> ')
+        if(choice == '1'):
+            hello_World()
+        elif(choice == '2'):
+            num_one = input('Enter the first number: ')
+            num_two = input('Enter the second number: ')
+            greatest_Num(num_one, num_two)
+        elif(choice == '3'):
+            roll_Dice()
+        elif(choice == '4'):
+            loop = False
+        else:
+            print('You did not enter a valid option. Try Again.')
+
+if(__name__ == '__main__'):
+    main()
